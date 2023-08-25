c33da8e5c7511ee503d2ccb9a00a077e7e0de752
vishav-pathania <vishavpathania40@gmail.com>
2023-08-25 21:12:42 +0530

    add buttons to switch banners and added cart logo to navbar

--- components/Banner.js
+++ components/Banner.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-const images = ['/banner1.avif', '/banner2.avif', '/banner3.jpg']; // Replace with your image URLs
+const images = ['/banner1.avif', '/banner2.avif', '/banner3.jpg', 'banner4.avif']; // Replace with your image URLs
 
 const Banner = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
   useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
+    const timer = setInterval(goToNextImage, 5000);
 
     return () => {
       clearInterval(timer);
@@ -17,7 +24,19 @@ const Banner = () => {
 
   return (
     <div className="relative">
-      <img src={images[currentImageIndex]} alt="Banner" className="w-full h-auto" />
+      <Link href="/#">
+        
+        <img src={images[currentImageIndex]} alt="Banner" className="w-full h-auto cursor-pointer" />
+        
+      </Link>
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full flex justify-between px-4">
+        <button onClick={goToPreviousImage} className="p-2 rounded white">
+          <img src="/left.png" alt="left" className="rounded w-3 h-5 lg:w-8 lg:h-11" />
+        </button>
+        <button onClick={goToNextImage} className="p-2 rounded white">
+        <img src="/right.png" alt="left" className="rounded w-3 h-5 lg:w-8 lg:h-11" />
+        </button>
+      </div>
     </div>
   );
 };
